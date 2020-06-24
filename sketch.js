6755e0bd7524b2b0bfbba98425cd704fb00d2cde
lakshay1907 <64905086+lakshay1907@users.noreply.github.com>
2020-06-24 22:20:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var database
+var drawing= [];
+function setup(){
+    canvas = createCanvas(400,400);
+    
+    database = firebase.database();
+}
+
+function draw(){
+    background(0);
+    if (mouseIsPressed){
+        var point = {
+            x: mouseX,
+            y: mouseY
+        }
+     drawing.push(point);
+    }
+    beginShape();
+    stroke(255);
+    strokeWeight(4);
+    noFill();
+  for  (var i =0; i < drawing.length; i++) {
+        vertex(drawing[i].x,drawing[i].y)
+}
+endShape();
+}
+
+
